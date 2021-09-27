cd1b0fd6b2371e4843feb87756343119ce14ac7a
MadhaviSamikkannu <75524555+MadhaviSamikkannu@users.noreply.github.com>
2021-09-27 22:41:01 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -19,17 +19,15 @@ domain = get_command_line_argument
 dns_raw = File.readlines("zone")
 ##
 
- def parse_dns(dns_raw1)
-  domain_record = Hash.new
-   dns_raw1.
-    delete_if { |line| line.empty? }.map { |line| line.strip.split(", ") }.
-    delete_if do |record|
-   end.
-    each_with_object({}) do |record, records|
+def parse_dns(dns_raw1)
+ domain_record = Hash.new
+ dns_raw1.
+  delete_if { |line| line.empty? }.map { |line| line.strip.split(", ") }.
+  each_with_object({}) do |record, records|
     domain_record[record[1]] = { type: record[0], target: record[2] }
-    end
-   domain_record
- end
+  end
+ domain_record
+end
 
 
 def resolve(dns_records, lookup_chain, domain_record)
@@ -37,16 +35,16 @@ def resolve(dns_records, lookup_chain, domain_record)
     if (!row)
         lookup_chain = []
         lookup_chain << "Error: record not found for  " + domain_record
-    elsif row[:type] == "A"
-        lookup_chain << row[:target]
     elsif row[:type] == "CNAME"
         lookup_chain << row[:target]
         resolve(dns_records, lookup_chain, row[:target])
+    elsif row[:type] == "A"
+        lookup_chain << row[:target]
     else
         puts "Error"
+        return lookup_chain
     end
-      return lookup_chain
-end
+  end
 
 
 # To complete the assignment, implement `parse_dns` and `resolve`.

3416762c4bf54ea12e144ef358215db7bd3b4da6
MadhaviSamikkannu <75524555+MadhaviSamikkannu@users.noreply.github.com>
2021-09-28 16:25:18 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -22,7 +22,7 @@ dns_raw = File.readlines("zone")
 def parse_dns(dns_raw1)
  domain_record = Hash.new
  dns_raw1.
-  delete_if { |line| line.empty? }.map { |line| line.strip.split(", ") }.
+  delete_if { |line| line.strip.empty? }.map { |line| line.strip.split(", ") }.
   each_with_object({}) do |record, records|
     domain_record[record[1]] = { type: record[0], target: record[2] }
   end
@@ -32,19 +32,19 @@ end
 
 def resolve(dns_records, lookup_chain, domain_record)
  row = dns_records[domain_record]
-    if (!row)
+ puts dns_records
+  if (!row)
         lookup_chain = []
         lookup_chain << "Error: record not found for  " + domain_record
-      elsif row[:type] == "A"
+  elsif row[:type] == "A"
         lookup_chain << row[:target]
-    elsif row[:type] == "CNAME"
+  elsif row[:type] == "CNAME"
         lookup_chain << row[:target]
         resolve(dns_records, lookup_chain, row[:target])
-    else
-        puts "Error"
+  else
         return lookup_chain
-    end
   end
+end
 
 
 # To complete the assignment, implement `parse_dns` and `resolve`.

7935c8a59856ae185ddcc4f6d3274cd7e465f350
MadhaviSamikkannu <75524555+MadhaviSamikkannu@users.noreply.github.com>
2021-10-04 20:13:24 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -1,5 +1,4 @@
 def get_command_line_argument
-
   if ARGV.empty?
     puts "Usage: ruby lookup.rb <domain>"
     exit
@@ -11,28 +10,28 @@ domain = get_command_line_argument
 dns_raw = File.readlines("zone")
 
 def parse_dns(dns_raw1)
-      domain_record = Hash.new
-      dns_raw1.
-       delete_if { |line| line.strip.empty? }.map { |line| line.strip.split(", ") }.
-         each_with_object({}) do |record, records|
-         domain_record[record[1]] = { type: record[0], target: record[2] }
-      end
-      domain_record
+  domain_record = Hash.new
+  dns_raw1.
+    delete_if { |line| line.strip.empty? }.map { |line| line.strip.split(", ") }.
+    each_with_object({}) do |record, records|
+    domain_record[record[1]] = { type: record[0], target: record[2] }
+  end
+  domain_record
 end
 
 def resolve(dns_records, lookup_chain, domain_record)
-   row = dns_records[domain_record]
-   if (!row)
-     lookup_chain = []
-     lookup_chain << "Error: record not found for  " + domain_record
-   elsif row[:type] == "A"
-     lookup_chain << row[:target]
-   elsif row[:type] == "CNAME"
-     lookup_chain << row[:target]
-      resolve(dns_records, lookup_chain, row[:target])
-   else
-      return lookup_chain
-   end
+  row = dns_records[domain_record]
+  if (!row)
+    lookup_chain = []
+    lookup_chain << "Error: record not found for  " + domain_record
+  elsif row[:type] == "A"
+    lookup_chain << row[:target]
+  elsif row[:type] == "CNAME"
+    lookup_chain << row[:target]
+    resolve(dns_records, lookup_chain, row[:target])
+  else
+    return lookup_chain
+  end
 end
 
 dns_records = parse_dns(dns_raw)
